75eaaad45c835afc2649eb3615559eaa2180da6f
carloskauan <carloskauan190@gmail.com>
2021-10-15 12:15:56 -0300

    Lógica Finalizada

---
+++ app/src/main/java/com/example/calculadora/Calcular.kt
@@ -0,0 +1,23 @@
+package com.example.calculadora
+
+class Calcular {
+    fun somar(N1 : Int, N2 : Int): Int{
+        val result = N1 + N2
+        return(result)
+    }
+
+    fun subtrair(N1: Int, N2: Int): Int{
+        val result = N1 - N2
+        return(result)
+    }
+
+    fun multiplicar(N1: Int, N2: Int) : Int{
+        val result = N1 * N2
+        return(result)
+    }
+
+    fun dividir(N1: Int, N2 : Int) : Int {
+        val result = N1 / N2
+        return(result)
+    }
+}
--- app/src/main/java/com/example/calculadora/MainActivity.kt
+++ app/src/main/java/com/example/calculadora/MainActivity.kt
@@ -2,10 +2,42 @@ package com.example.calculadora
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.service.voice.VoiceInteractionSession
+import android.widget.Toast
+import com.example.calculadora.databinding.ActivityMainBinding
+import java.text.DecimalFormat
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.CalcButton.setOnClickListener{
+            calculateOperation()
+            //Toast
+            val toast = Toast.makeText(this, "Sucesso no Calculo", Toast.LENGTH_SHORT)
+            toast.show()
+        }
+    }
+    fun calculateOperation(){
+
+
+        val N1 = binding.Valor1.text.toString().toInt()
+        val N2 = binding.Valor2.text.toString().toInt()
+
+        val selectedId = binding.tipOptions.checkedRadioButtonId
+
+        val OperationTip = when (selectedId) {
+            R.id.option_sum -> Calcular().somar(N1,N2)
+            R.id.option_sub -> Calcular().subtrair(N1,N2)
+            R.id.option_multi -> Calcular().multiplicar(N1,N2)
+            else -> Calcular().dividir(N1,N2)
+        }
+
+        val result = DecimalFormat().format(OperationTip)
+        binding.ResultadoView.text = result
     }
 }
+
